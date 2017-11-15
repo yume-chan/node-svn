d80d3f940e47bfd493e9ab314fa1d9860977efec
Simon Chan <cnsimonchan@live.com>
2017-11-15 18:00:46 +0800

    Rename status_kind

--- src/node/status_kind.hpp
+++ src/node/status_kind.hpp
@@ -3,7 +3,7 @@
 #include <node/v8.hpp>
 
 namespace node {
-namespace working_copy_status {
+namespace status_kind {
 void init(v8::Local<v8::Object>   exports,
           v8::Isolate*            isolate,
           v8::Local<v8::Context>& context);
