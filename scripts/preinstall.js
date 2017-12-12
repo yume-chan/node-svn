d8909da9fce87185f89a6dbd200d263d876cbe68
Simon Chan <cnsimonchan@live.com>
2017-12-12 18:37:07 +0800

    Add xcode settings

--- scripts/preinstall.js
+++ scripts/preinstall.js
@@ -53,6 +53,11 @@ const configuration = {
             "cflags_cc!": [
                 "-fno-rtti",
             ],
+            xcode_settings: {
+                GCC_ENABLE_CPP_EXCEPTIONS: "YES",
+                CLANG_CXX_LIBRARY: "libc++",
+                MACOSX_DEPLOYMENT_TARGET: "10.7",
+            },
             msvs_settings: {
                 VCCLCompilerTool: {
                     AdditionalOptions: [
